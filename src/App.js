ab9e75890f4f5c0f018c3488ed24a19e177d36d4
Jon Mico <jonmico@gmail.com>
2023-04-20 23:59:00 -0400

    set up initial layout for project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/* <img />  */}
+        <div className='category-body-container'></div>
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className='category-container'>
+        {/* <img />  */}
+        <div className='category-body-container'></div>
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className='category-container'>
+        {/* <img />  */}
+        <div className='category-body-container'></div>
+        <h2>Sneakers</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className='category-container'>
+        {/* <img />  */}
+        <div className='category-body-container'></div>
+        <h2>Womens</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className='category-container'>
+        {/* <img />  */}
+        <div className='category-body-container'></div>
+        <h2>Mens</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
