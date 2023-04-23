72182c23baed91af843c3cb8918ef3042af87be7
Jon Mico <jonmico@gmail.com>
2023-04-23 16:07:07 -0400

    add firebase utils folder, add firebase config code

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6l3_EDFYz3w-4eecLCbkjeqnMB1KHwZ8',
+  authDomain: 'crwn-clothing-db-2a103.firebaseapp.com',
+  projectId: 'crwn-clothing-db-2a103',
+  storageBucket: 'crwn-clothing-db-2a103.appspot.com',
+  messagingSenderId: '634523110547',
+  appId: '1:634523110547:web:bc6bb9b5c9bb5453155166',
+};
+
+const app = initializeApp(firebaseConfig);
