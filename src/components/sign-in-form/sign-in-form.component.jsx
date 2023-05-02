a96767f3c22bb5a36e734f8bd4c640e12259b2e2
Jon Mico <jonmico@gmail.com>
2023-05-02 12:24:41 -0400

    add styled components to SignInForm

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -9,7 +9,11 @@ import {
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
-import './sign-in-form.styles.scss';
+import {
+  SignUpContainer,
+  Header,
+  ButtonsContainer,
+} from './sign-in-form.styles.jsx';
 
 const defaultFormFields = {
   email: '',
@@ -52,8 +56,8 @@ const SignInForm = () => {
   };
 
   return (
-    <div className='sign-up-container'>
-      <h2>Already have an account?</h2>
+    <SignUpContainer>
+      <Header>Already have an account?</Header>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
@@ -73,7 +77,7 @@ const SignInForm = () => {
           name='password'
           value={password}
         />
-        <div className='buttons-container'>
+        <ButtonsContainer>
           <Button type='submit'>Sign In</Button>
           <Button
             type='button'
@@ -82,9 +86,9 @@ const SignInForm = () => {
           >
             Google Sign In
           </Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
